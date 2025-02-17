1d63b1c6687f96a95c683fa6c91c8d7f520e86a5
soumya-536 <71547769+soumya-536@users.noreply.github.com>
2025-02-17 14:07:59 +0530

    Update router.js

--- routes/router.js
+++ routes/router.js
@@ -3,6 +3,8 @@ const router = express.Router();
 
 const visitRoutes = require('../Controllers/Visitor');
 
+router.get('/getVisitors', visitRoutes.getVisitors);
+router.get('/getVisits', visitRoutes.getVisits);
 router.get('/visits', visitRoutes.getAllVisits);
 
 module.exports = router;
